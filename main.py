1e6c2c58bf870a790f80b50bb264bb9d1ac1f07e
brettlungal <lungalb@myumanitoba.ca>
2022-11-06 22:04:19 -0700

    playing audio in discord voice channel working

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 from constants import FFMPEG_OPTIONS
 import os
 from dotenv import load_dotenv
+import urllib
+import re
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
@@ -16,17 +18,18 @@ async def on_ready():
 async def boom(ctx):
     await ctx.send("https://www.twitch.tv/itsyourboyandrew1/clip/HeartlessWittyGoatWOOP-a93YUHFgWVNww0SQ")
 
-@bot.command
+@bot.command()
 async def fight(ctx):
-    search = "morpheus tutorials discord bot python"
+    search = "shoot to thrill lyrics"
     if ctx.message.author.voice == None:
         await ctx.reply("No Voice Channel", "You need to be in a voice channel to use this command!")
         return
 
     channel = ctx.message.author.voice.channel
     voice = discord.utils.get(ctx.guild.voice_channels, name=channel.name)
-    #TODO change below line
-    voice_client = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+    #TODO figure out what to do with line below
+    voice_clients = []
+    voice_client = discord.utils.get(voice_clients, guild=ctx.guild)
 
     if voice_client == None:
         voice_client = await voice.connect()
@@ -37,11 +40,11 @@ async def fight(ctx):
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search)
     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
 
-    await ctx.send("https://www.youtube.com/watch?v=" + video_ids[0])
-
     song = pafy.new(video_ids[0])  # creates a new pafy object
+    print(song)
     audio = song.getbestaudio()  # gets an audio source
     source = FFmpegPCMAudio(audio.url, **FFMPEG_OPTIONS)  # converts the youtube audio source into a source discord can use
+    print(type(source))
     voice_client.play(source)  # play the source
 
 @bot.command()
