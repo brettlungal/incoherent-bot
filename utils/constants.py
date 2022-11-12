27eb5691e59feb5a0fb8f4627c77dd455b116c41
brettlungal <lungalb@myumanitoba.ca>
2022-11-12 12:46:01 -0700

    add extra option to idea

--- utils/constants.py
+++ utils/constants.py
@@ -5,5 +5,6 @@ IDEA_LIST = [
     {"name": 'Athena Emmissary', "desc": "Throw up an Athena flag and grind out a legend of the veil to get the funny green chest and Athena rep"},
     {"name": 'Gold Hoarders Vault', "desc": "Throw up a gold hoarders flag, get a vault quest and go get some booty"},
     {"name": 'World Event', "desc": "Go do whatever world event is up - if none are up do the first one that appears or server hop"},
-    {"name": 'Fort of The Dammed', "desc": "Grab the required lantern colors and initiate a fort of the dammed and wait for the violence and riches to unfold"}
+    {"name": 'Fort of The Dammed', "desc": "Grab the required lantern colors and initiate a fort of the dammed and wait for the violence and riches to unfold"},
+    {"name": "Slithery Sneaky Snake", "desc": "Troll on other players, tuck on their boats and cause general mayhem"}
     ]
